87da3744534aa35e5707e918099cedb134a584a7
Regis Santos <regis42santos@gmail.com>
2021-06-04 15:54:11 -0300

    refactor is done

--- src/views/UserTodo.vue
+++ src/views/UserTodo.vue
@@ -26,12 +26,14 @@
                     <div>
                         <i
                             v-if="todo.is_done"
-                            class="far fa-check-square"
+                            class="far fa-check-square is-link"
+                            @click="toggleDone(todo, false)"
                         ></i>
 
                         <i
                             v-else
-                            class="far fa-square"
+                            class="far fa-square is-link"
+                            @click="toggleDone(todo, true)"
                         ></i>
                     </div>
                 </div>
@@ -47,13 +49,42 @@ export default {
             user: {},
         };
     },
-
     mounted() {
-        const userId = this.$route.params.id;
-        fetch(`http://localhost:8000/api/v1/core/users/${userId}`)
-            .then(response => response.json())
-            .then(res => this.user = res);
+        this.getData();
     },
-
+    methods: {
+        getData() {
+            const userId = this.$route.params.id;
+            fetch(`http://localhost:8000/api/v1/core/users/${userId}`)
+                .then(response => response.json())
+                .then(res => this.user = res);
+        },
+        toggleDone(obj, value) {
+            const todoId = obj.id;
+            fetch(`http://localhost:8000/api/v1/todo/todos/${todoId}/done`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    is_done: value
+                })
+            })
+            .then(response => response.json())
+            .then(() => {
+                this.getData();
+            })
+        }
+    }
 };
 </script>
+
+<style scoped>
+.is-link {
+    cursor: pointer;
+}
+.is-link:hover {
+    cursor: pointer;
+    color: #0d6efd;
+}
+</style>
